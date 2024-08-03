9b8723398c5e069bf57a6ae284d33f51d3e2b248
Felipe Londoño Cardona <f.londono@refacil.com>
2024-08-03 12:02:59 -0500

    style(home): :lipstick: Improve main image size

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,16 +1,41 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Skills from "@/components/Skills";
 import { IHomeProps } from "@/types/props/home";
 
 export default function Home(props: IHomeProps) {
   const { content } = props;
+  const [innerWidth, setInnerWidth] = useState<number>(window.innerWidth);
+
+  const handleResize = () => {
+    setInnerWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const getImageDimensions = () => {
+    if (innerWidth <= 768) {
+      return { width: 600, height: 400 };
+    } else if (innerWidth <= 1500) {
+      return { width: 300, height: 600 };
+    } else {
+      return { width: 380, height: 700 };
+    }
+  };
+
+  const { width, height } = getImageDimensions();
+
   return (
     <section
       id="home"
       data-scroll-section
       className="w-screen min-h-screen h-screen bg-transparent relative"
     >
-      <div className="absolute top-20 lg:top-[4rem] 2xl:top-14 left-[14%] sm:left-[7%] lg:left-[6%] text-jazzberry-jam-300 font-bold ">
+      <div className="absolute top-20 lg:top-[4rem] 2xl:top-14 left-[14%] sm:left-[7%] lg:left-[6%] text-jazzberry-jam-300 font-bold">
         <Skills words={content?.skills} />
       </div>
       <div className="w-[50%] sm:w-auto absolute top-20 lg:top-[4rem] 2xl:top-14 right-0 sm:right-[7%] lg:right-[4%] text-jazzberry-jam-300 font-bold">
@@ -21,7 +46,7 @@ export default function Home(props: IHomeProps) {
       <div
         data-scroll
         data-scroll-speed="5"
-        className="absolute top-1/3 transform -translate-y-1/3 sm:bottom-12 w-full h-auto flex flex-col items-center justify-end z-10 overflow-hidden space-y-[-3.3vw]"
+        className="absolute top-[35%] transform -translate-y-1/3 sm:bottom-12 w-full h-auto flex flex-col items-center justify-end z-10 overflow-hidden space-y-[-3.3vw]"
       >
         <h1 className="tw-primary-title text-jazzberry-jam-400 font-BeckanPersonal leading-none text-[17vw] xl:text-[15vw] 2xl:text-[13vw] overflow-hidden">
           {content?.name.toUpperCase()}
@@ -30,20 +55,16 @@ export default function Home(props: IHomeProps) {
           {content?.name.toUpperCase()}
         </h1>
       </div>
-      <div className="absolute w-[90%] max-w-[270px] md:max-w-[300px] lg:max-w-[340px] 2xl:max-w-[460px] h-[70%] lg:h-[95%] px-1 bottom-0 left-1/2 transform -translate-x-1/2 z-20">
-        <div
-          data-scroll
-          data-scroll-speed="1"
-          className="w-full h-full relative"
-        >
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-20">
+        <div data-scroll data-scroll-speed="1" className="relative">
           <Image
             src={content?.image}
             alt="Isabella"
-            fill
-            sizes="(max-width: 600px) 100vw, 600px"
+            width={width}
+            height={height}
             priority
             quality={100}
-            className="main-image blur-in"
+            className="main-image blur-in w-[95%] 2xl:w-full h-auto"
           />
         </div>
       </div>
